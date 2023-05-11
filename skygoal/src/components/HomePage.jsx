c922f8fa788ff918478327d01dce6e21c6b2a00c
sulthan441 <mohammedsulthan441@gmail.com>
2023-05-11 17:03:53 +0530

    done completely

--- skygoal/src/components/HomePage.jsx
+++ skygoal/src/components/HomePage.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function HomePage() {
+  const classes = useStyles();
+
   return (
-    <div>
-        <h1>HomePage</h1>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            My App
+          </Typography>
+          <Button component={Link} to="/login" color="inherit">Login</Button>
+          <Button component={Link} to="/signup" color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <h1>Welcome to my homepage</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
